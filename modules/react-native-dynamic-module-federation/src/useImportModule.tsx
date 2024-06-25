03c0024fe6c6b388ed0500cc4feff255ac6cda20
Joondong <kwonjoondong@gmail.com>
2024-06-26 08:40:03 +0900

    react-native-dynamic-module-federation > useImportModule이 매번 익명 컴포넌트를 반환해서 상태를 가지지 못하는 문제 수정 (uri가 바뀌는 경우에만 익명 컴포넌트를 반환하고, 그렇지 않다면 메모된 기존 컴포넌트를 반환하도록 변경)

--- modules/react-native-dynamic-module-federation/src/useImportModule.tsx
+++ modules/react-native-dynamic-module-federation/src/useImportModule.tsx
@@ -1,55 +1,42 @@
-import React, {
-  useContext,
-  useEffect,
-  useRef,
-  useState,
-  type PropsWithChildren,
-} from "react";
+import React, { useContext, useEffect, useRef, useState } from "react";
 import { Federated } from "@callstack/repack/client";
 import { Context } from "./ImportModuleProvider";
 
 export interface Containers {
-  [name: string]: string; // name: version
+  [name: string]: string; // name: uri
 }
 
-const Null: React.FunctionComponent = () => null;
+const Null = () => null;
 
-export const useImportModule = (
+export function useImportModule<P = any>(
   containerName: string,
   moduleName: string
-):
-  | React.LazyExoticComponent<React.ComponentType<any>>
-  | React.FunctionComponent<{}> => {
+): (props: P) => JSX.Element | null {
   const { containers } = useContext(Context);
 
-  const version = containers[containerName];
+  const uri = useRef(containers[containerName]);
 
-  const prevVersion = useRef(version);
-  const [Lazy, setLazy] =
-    useState<React.LazyExoticComponent<React.ComponentType<any>>>();
+  const [Lazy, setLazy] = useState<(props: any) => JSX.Element>();
 
   useEffect(() => {
-    setLazy(
-      (
-        prev: React.LazyExoticComponent<React.ComponentType<any>> | undefined
-      ) => {
-        if (version) {
-          if (!prev || prevVersion.current !== version) {
-            prevVersion.current = version;
-            return React.lazy(() =>
-              Federated.importModule(containerName, moduleName)
-            );
-          }
-        }
-        return prev;
-      }
-    );
-  }, [containerName, version]);
+    setLazy((prev?: (props: P) => JSX.Element) => {
+      const newUri = containers[containerName];
+      if (uri) {
+        if (!prev || uri.current !== newUri) {
+          uri.current = newUri;
+          // 익명 컴포넌트가 아니면 밖에서 컴포넌트 내용이 변경된 것을 인식하지 못한다.
+          const NewLazy = React.lazy(() =>
+            Federated.importModule(containerName, moduleName)
+          );
 
-  // 익명 컴포넌트가 아니면 밖에서 컴포넌트 내용이 변경된 것을 인식하지 못한다.
-  return (props: any) => {
-    return Lazy ? <Lazy {...props} /> : <Null />;
-  };
-};
+          return (props: P) => <NewLazy {...props} />;
+        }
+      }
+      return prev;
+    });
+  }, [containers, containerName, uri]);
+
+  return Lazy ?? Null;
+}
 
 export default useImportModule;
