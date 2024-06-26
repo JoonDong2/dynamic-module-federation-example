03607a3b2656058f563501973bdc7fc188336c48
Joondong <kwonjoondong@gmail.com>
2024-06-27 03:49:33 +0900

    entry > 호스트로 작동할 수 있도록 수정

--- apps/entry/App.tsx
+++ apps/entry/App.tsx
@@ -1,15 +1,30 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- */
-
 import React from 'react';
 import Entry from './src/Entry';
+import {
+  hocPipe,
+  useContainers,
+  withErrorBoundary,
+  withQueryClient,
+  withSuspense,
+} from 'shared';
+import {ImportModuleProvider} from 'react-native-dynamic-module-federation';
+import {QueryClient} from '@tanstack/react-query';
+import {Text} from 'react-native';
 
-function App() {
-  return <Entry />;
-}
+const App = () => {
+  const containers = useContainers({suspense: false});
 
-export default App;
+  return (
+    <ImportModuleProvider containers={containers}>
+      <Entry />
+    </ImportModuleProvider>
+  );
+};
+
+const queryClient = new QueryClient();
+
+export default hocPipe([
+  withQueryClient(queryClient),
+  withErrorBoundary(<Text>Containers 오류</Text>),
+  withSuspense(<Text>Containers 로딩중</Text>),
+])(<App />);
