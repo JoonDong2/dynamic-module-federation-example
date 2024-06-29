cd27d7a38f7a63cc834f75331d93f87cf4ef587b
Joondong <kwonjoondong@gmail.com>
2024-06-29 20:22:56 +0900

    shared > resolver 제거

--- shared/src/index.ts
+++ shared/src/index.ts
@@ -1,5 +1,4 @@
 export { getDevContainers, fetchContainers } from './containers';
 export { getLocalhost } from './localhost';
-export { generateResolver } from './resolver';
 export * from './hocs';
 export * from './hooks';
