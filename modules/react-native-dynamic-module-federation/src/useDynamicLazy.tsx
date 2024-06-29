6b77d322eaefd0bde8f76294ab31e5c20ae020e8
Joondong <kwonjoondong@gmail.com>
2024-06-30 05:40:39 +0900

    react-native-dynamic-module-federation > useDynamicLazy wrapper 추가

--- modules/react-native-dynamic-module-federation/src/useDynamicLazy.tsx
+++ modules/react-native-dynamic-module-federation/src/useDynamicLazy.tsx
@@ -1,4 +1,10 @@
-import React, { useContext, useEffect, useRef, useState } from 'react';
+import React, {
+  useContext,
+  useEffect,
+  useRef,
+  useState,
+  type PropsWithChildren,
+} from 'react';
 import { Federated } from '@callstack/repack/client';
 import { Context } from './DynamicImportProvider';
 
@@ -6,8 +12,12 @@ const Null = () => null;
 
 export function useDynamicLazy<P = any>(
   containerName: string,
-  moduleName: string
+  moduleName: string,
+  options?: {
+    wrapper: (props: PropsWithChildren) => React.ReactElement;
+  }
 ): (props: P) => JSX.Element | null {
+  const Wrapper = options?.wrapper;
   const { containers } = useContext(Context);
 
   const uri = useRef(containers?.[containerName]);
@@ -26,11 +36,22 @@ export function useDynamicLazy<P = any>(
             Federated.importModule(containerName, moduleName)
           );
 
-          return (props: P) => <NewLazy {...props} />;
+          return (props: P) => {
+            if (Wrapper) {
+              return (
+                <Wrapper>
+                  <NewLazy {...props} />
+                </Wrapper>
+              );
+            }
+            return <NewLazy {...props} />;
+          };
         }
       }
       return prev;
     });
+    // containers가 변경될 때만 실행되면 되므로, Wrapper를 의존성 배열에 등록할 필요는 없다.
+    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [containers, containerName, moduleName]);
 
   return Lazy ?? Null;
