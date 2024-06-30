c5e5dd36ac7611b3ed4989f8ca31cd11d94804d0
Joondong <kwonjoondong@gmail.com>
2024-07-01 04:16:51 +0900

    react-native-dynamic-module-federation > useDynamicLazy 컨테이너 변경에 따라 같이 업데이트되도록 변경

--- modules/react-native-dynamic-module-federation/src/useDynamicLazy.tsx
+++ modules/react-native-dynamic-module-federation/src/useDynamicLazy.tsx
@@ -2,9 +2,7 @@ import React, {
   Component,
   Suspense,
   useContext,
-  useEffect,
   useRef,
-  useState,
   type FunctionComponent,
   type PropsWithChildren,
   type ReactElement,
@@ -13,6 +11,7 @@ import React, {
 import { Federated } from '@callstack/repack/client';
 import { Context } from './DynamicImportProvider';
 import { ErrorBoundary } from 'react-error-boundary';
+import useMemoWithPrev from './useMemoWithPrev';
 
 const Null = () => null;
 
@@ -33,21 +32,19 @@ export function useDynamicLazy<P = any>(
 
   const uri = useRef(containers?.[containerName]);
 
-  const [Lazy, setLazy] = useState<(props: any) => JSX.Element>();
-
-  useEffect(() => {
-    setLazy((prev?: (props: P) => JSX.Element) => {
+  const Lazy = useMemoWithPrev<undefined | ((props: P) => JSX.Element)>(
+    (prev) => {
       const newUri = containers?.[containerName];
       if (newUri) {
         if (!prev || uri.current !== newUri) {
           uri.current = newUri;
           // 익명 컴포넌트가 아니면 밖에서 컴포넌트 내용이 변경된 것을 인식하지 못한다.
           // Promise를 대신 던져준다.
-          const NewLazy = React.lazy(() =>
+          const NewOriginLazy = React.lazy(() =>
             Federated.importModule(containerName, moduleName)
           );
 
-          return (props: P) => {
+          const NewLazy = (props: P) => {
             return (
               [
                 [ErrorBoundary, options?.fallbacks?.error],
@@ -63,16 +60,18 @@ export function useDynamicLazy<P = any>(
                 if (!React.isValidElement(fallback)) return acc;
                 return <Wrapper fallback={fallback}>{acc}</Wrapper>;
               },
-              <NewLazy {...props} />
+              <NewOriginLazy {...props} />
             );
           };
+
+          return NewLazy;
         }
       }
       return prev;
-    });
-    // containers가 변경될 때만 실행되면 되므로, Wrapper를 의존성 배열에 등록할 필요는 없다.
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [containers, containerName, moduleName]);
+      // 어차피 버전때문에 prev가 반환되지만, containers가 변경될 때 상태만 가져오면 되므로, fallbacks를 의존성 배열에 등록할 필요는 없다.
+    },
+    [containers, containerName, moduleName]
+  );
 
   return Lazy ?? Null;
 }
