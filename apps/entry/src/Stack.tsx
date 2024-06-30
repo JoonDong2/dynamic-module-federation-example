06bb624694d9a9c50f483347ad5a78a9922c2e50
Joondong <kwonjoondong@gmail.com>
2024-06-30 10:58:56 +0900

    entry > useDynamicLazy > fallbacks 적용

--- apps/entry/src/Stack.tsx
+++ apps/entry/src/Stack.tsx
@@ -3,6 +3,7 @@ import {createStackNavigator} from '@react-navigation/stack';
 import Tab from './Tab';
 import {RootStackParamList} from 'shared/navigation';
 import {useDynamicLazy} from 'react-native-dynamic-module-federation';
+import {Text} from 'react-native';
 
 const StackNavigator = createStackNavigator<
   RootStackParamList & {
@@ -14,6 +15,12 @@ const Stack = () => {
   const AlphabetDetail = useDynamicLazy(
     'alphabet',
     './alphabet/screens/Detail',
+    {
+      fallbacks: {
+        suspense: <Text>Alphabet Detail 로딩중</Text>,
+        error: <Text>Alphabet Detail 오류</Text>,
+      },
+    },
   );
   return (
     <StackNavigator.Navigator screenOptions={{headerShown: false}}>
--- apps/entry/src/Tab.tsx
+++ apps/entry/src/Tab.tsx
@@ -2,11 +2,17 @@ import React from 'react';
 import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
 import {TabParamList} from 'shared/navigation';
 import {useDynamicLazy} from 'react-native-dynamic-module-federation';
+import {Text} from 'react-native';
 
 const TabNavigator = createBottomTabNavigator<TabParamList>();
 
 const Tab = () => {
-  const AlphabetMain = useDynamicLazy('alphabet', './alphabet/screens/Main');
+  const AlphabetMain = useDynamicLazy('alphabet', './alphabet/screens/Main', {
+    fallbacks: {
+      suspense: <Text>Alphabet Main 로딩중</Text>,
+      error: <Text>Alphabet Main 오류</Text>,
+    },
+  });
   return (
     <TabNavigator.Navigator screenOptions={{headerShown: false}}>
       <TabNavigator.Screen name="alphabet:Main" component={AlphabetMain} />
