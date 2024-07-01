478e7941a21502d45666ccc875a9a0cd619e7d09
Joondong <kwonjoondong@gmail.com>
2024-07-02 03:29:35 +0900

    install-all.sh > 의존성 설치 순서 변경

--- scripts/install-all.sh
+++ scripts/install-all.sh
@@ -5,14 +5,6 @@ ROOT=$SCRIPT_DIR/..
 
 echo "apps 폴더에 포함된 모든 앱과 containers-server의 의존성을 설치합니다."
 
-# apps 폴더의 모든 하위 폴더에서 npm install 실행
-for dir in $ROOT/apps/*; do
-  if [ -d "$dir" ]; then
-    echo "Running npm install in $dir"
-    (cd "$dir" && npm install)
-  fi
-done
-
 # modules 폴더의 모든 하위 폴더에서 npm install 실행
 for dir in $ROOT/modules/*; do
   if [ -d "$dir" ]; then
@@ -26,6 +18,14 @@ if [ -d "$ROOT/shared" ]; then
   echo "Running npm install in shared"
   (cd "$ROOT/shared" && npm install)
 fi
+
+# apps 폴더의 모든 하위 폴더에서 npm install 실행
+for dir in $ROOT/apps/*; do
+  if [ -d "$dir" ]; then
+    echo "Running npm install in $dir"
+    (cd "$dir" && npm install)
+  fi
+done
 
 # server 폴더에서 npm install 실행
 if [ -d "$ROOT/containers-server" ]; then
